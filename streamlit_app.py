409981c4b0b1c78b3fde8efd4d977f9911ed51d3
Chandra Prakash <cchhandu1@gmail.com>
2024-04-27 16:27:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,19 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+from transformers import pipeline
+from PIL import Image
+import torch
 
-"""
-# Welcome to Streamlit!
+##BLIP
+# Create the caption pipeline
+initial_caption = pipeline('image-to-text', model="Salesforce/blip-image-captioning-large")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Display the image using Streamlit
+uploaded_image = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+if uploaded_image is not None:
+    image= Image.open(uploaded_image)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+# Generate the caption
+if st.button("Generate Caption"):
+    captions = initial_caption(image)
+    st.write(captions[0]['generated_text'])
